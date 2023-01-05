38e9663d8046f34f7b9a44aa7ab2e18fce5e28b9
Giovanna <giovannamsantanaa@gmail.com>
2023-01-05 10:50:25 -0300

    Iniciando estrutura de pacote model, com a entidade Game

---
+++ src/main/java/me/dio/diogameawards/domain/model/Game.java
@@ -0,0 +1,53 @@
+package me.dio.diogameawards.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "GAMES") // representar a classe como uma tabela no bd
+public class Game {
+	
+	@Id
+	// Incrementar de chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // Long com L aceita o null
+	private String name;
+	private String description;
+	private String cover;
+	private long votes; // long l o valor default é 0
+
+	//Getters e setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCover() {
+		return cover;
+	}
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+	public long getVotes() {
+		return votes;
+	}
+	public void setVotes(long votes) {
+		this.votes = votes;
+	}
+
+
+}
